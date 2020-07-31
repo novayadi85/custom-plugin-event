caff52a754a761ae19f3109c0e2133abc8fa9ecc
Komang Novayadi <novayadi85@gmail.com>
2020-08-01 01:45:33 +0800

    Update and rename Event.js to Events.js

--- src/resolvers/Query/Events.js
+++ src/resolvers/Query/Events.js
@@ -1,6 +1,6 @@
 import { decodeShopOpaqueId } from "../../xforms/id.js";
 
-export default async function event(_, args, context, info) {
+export default async function events(_, args, context, info) {
     const { shopId: opaqueShopId, productId , variantId } = args;
     
     const shopId = decodeShopOpaqueId(opaqueShopId);
